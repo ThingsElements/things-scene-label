8df59cb41e0022e9a1011172e46a61d788e81b55
Hearty Oh <heartyoh@hatiolab.com>
2019-04-02 01:04:58 +0900

    locale

--- locales/index.js
+++ locales/index.js
@@ -1,12 +1,14 @@
 export default {
-    'en-US': {
-      'keyword': 'keyword'
-    },
-    'ko-KR': {
-      'keyword': '키워드'
-    },
-    'zh-CN': {
-      'keyword': 'keyword'
-    }
+  "en-US": {
+    "label.monochrome": "monochrome",
+    "label.symbol": "symbol"
+  },
+  "ko-KR": {
+    "label.monochrome": "단색",
+    "label.symbol": "심볼"
+  },
+  "zh-CN": {
+    "label.monochrome": "monochrome",
+    "label.symbol": "symbol"
   }
-  
+};
