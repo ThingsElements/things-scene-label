9b35330af1f2ed50a80be1b8479f2897d77f69c0
Hearty Oh <heartyoh@hatiolab.com>
2019-04-02 01:35:42 +0900

    fix template

--- templates/barcode.js
+++ templates/barcode.js
@@ -1,18 +1,18 @@
-import icon from './barcode.png';
+import icon from "./barcode.png";
 
 export default {
-  type: 'barcode',
-  description: 'barcode',
-  group: 'IoT',
+  type: "barcode",
+  description: "barcode",
+  group: "IoT",
   /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
   icon,
   model: {
-    type: 'barcode',
+    type: "barcode",
+    symbol: "code39",
+    text: "1234567890",
     left: 10,
     top: 10,
-    width: 100,
-    height: 100,
-    fillStyle: 'cyan',
-    strokeStyle: 'darkgray'
+    width: 500,
+    height: 100
   }
 };
